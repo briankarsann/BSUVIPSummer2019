22cee0ad01222cfaca28912f51f74443dd7a5f15
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 21:35:04 -0600

    Removed Java code from vip.py

--- vip.py
+++ vip.py
@@ -176,39 +176,6 @@ class Vehicle:
 	@number_of_wheels.setter
 	def number_of_wheels(self, number):
 		self.__number_of_wheels = number 
-		
-# Java
-
-public static void main(String[] args) { 
-		System.out.println("Hey guys ");
-		
-import java.util.Scanner;
-public static void main(String[] args) { 
-    System.out.println("What is your name?");
-		
-//type identifier = new type()
-    Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
-		
-System.out.println("Hello "+ name);
-
-String string = new String("This is a string");
-String easier = "Wow, that was easy!";
-System.out.println(easier");
-	}
-
-}
-
-passwordFile = open('SecretPasswordFile.txt')
-secretPassword = passwordFile.read()
-print('Enter your password.')
-typedPassword = input()
-if typedPassword == secretPassword:
-	print('Access Granted')
-	if typedPassword == '12345' :
-		print('That password is one that an idiot puts on their luggage.')
-else:
-	print('Access Denied');
 	
 # Python 
 
