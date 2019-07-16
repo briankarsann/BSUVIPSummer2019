2a6c5c1dc778e4c434b8cf109ddf543fd08f9d97
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 22:09:55 -0600

    Fixed strings at end

--- vip.java
+++ vip.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class VIP
 {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args)
+	{
 		System.out.println("Hey guys ");
-    	System.out.println("What is your name?");
-   		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is your name?");
+		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
-		
+
 		System.out.println("Hello " + name);
 
 		String string = new String("This is a string");
@@ -21,9 +22,9 @@ public class VIP
 		String secretPassword = "";
 		if (passwordFile.exists() && passwordFile.isFile())
 		{
-			try 
+			try
 			{
-				Scanner scanPass = new Scanner(passwordFile); 
+				Scanner scanPass = new Scanner(passwordFile);
 				while (scanPass.hasNextLine()) // Cycle through all lines and add them to output with a line break
 				{
 					String passLine = scanPass.nextLine();
@@ -33,23 +34,25 @@ public class VIP
 			}
 			catch (FileNotFoundException e) // Catch proper exception if file isn't found and print message
 			{
-				System.out.println("File not found. The file has likely been moved or deleted since the book was created.");	
+				System.out.println(
+						"File not found. The file has likely been moved or deleted since the book was created.");
 				System.exit(1);
 			}
 		}
 		System.out.println("Enter your password.");
 		Scanner typedPassword = new Scanner(System.in);
 		String password = typedPassword.nextLine();
-		if(password == secretPassword)
+		if (password == secretPassword)
 		{
 			System.out.println("Access Granted");
 		}
-		else if(password.equals("12345"))
+		else if (password.equals("12345"))
 		{
-			print("That password is one that an idiot puts on their luggage.")
+			System.out.println("That password is one that an idiot puts on their luggage.");
 		}
 		else
 		{
-			print("Access Denied");
+			System.out.println("Access Denied");
 		}
+	}
 }
