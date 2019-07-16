6175babc04dea44b5ce5493ae55f78fa63445784
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 21:53:02 -0600

    Further indentation fixes

--- vip.java
+++ vip.java
@@ -4,32 +4,31 @@ public class VIP
 {
 
 	public static void main(String[] args) { 
-	System.out.println("Hey guys ");
-    	System.out.println("What is your name?");
-   	Scanner scanner = new Scanner(System.in);
-	String name = scanner.nextLine();
+		System.out.println("Hey guys ");
+    		System.out.println("What is your name?");
+   		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
 		
-	System.out.println("Hello " + name);
+		System.out.println("Hello " + name);
 
-	String string = new String("This is a string");
-	String easier = "Wow, that was easy!";
-	System.out.println(easier);
+		String string = new String("This is a string");
+		String easier = "Wow, that was easy!";
+		System.out.println(easier);
 
-	passwordFile = open("SecretPasswordFile.txt")
-	secretPassword = passwordFile.read()
-	print('Enter your password.')
-	typedPassword = input()
-	if typedPassword == secretPassword:
-	{
-		print('Access Granted')
-	}
-	else if typedPassword == '12345'
-	{
-		print("That password is one that an idiot puts on their luggage.")
-	}
-	else
-	{
-		print("Access Denied");
-	}
-
+		passwordFile = open("SecretPasswordFile.txt")
+		secretPassword = passwordFile.read()
+		print('Enter your password.')
+		typedPassword = input()
+		if typedPassword == secretPassword:
+		{
+			print('Access Granted')
+		}
+		else if typedPassword == '12345'
+		{
+			print("That password is one that an idiot puts on their luggage.")
+		}
+		else
+		{
+			print("Access Denied");
+		}
 }
