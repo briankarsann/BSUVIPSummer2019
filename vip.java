230525ac223fbb639431a71fe2cae5eb0a9ccecf
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 21:47:32 -0600

    Formatted Java code - changed indents

--- vip.java
+++ vip.java
@@ -23,7 +23,7 @@ public class VIP
 		print('Access Granted')
 		if typedPassword == '12345' :
 		print('That password is one that an idiot puts on their luggage.')
-else:
+		else:
 	print('Access Denied');
 
 }
