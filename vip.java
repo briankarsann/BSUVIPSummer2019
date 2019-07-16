8fce429f4197f0cf31055882c65627c78e433c1b
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 21:48:42 -0600

    Fixed if statement in Java code

--- vip.java
+++ vip.java
@@ -21,8 +21,10 @@ public class VIP
 		typedPassword = input()
 		if typedPassword == secretPassword:
 		print('Access Granted')
-		if typedPassword == '12345' :
-		print('That password is one that an idiot puts on their luggage.')
+		if typedPassword == '12345'
+		{
+			print('That password is one that an idiot puts on their luggage.')
+		}
 		else:
 			print('Access Denied');
 
