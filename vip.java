42e9e8a04232c1693586639c7dc2093221641a06
Forrest Burt <forrestburt@u.boisestate.edu>
2019-07-15 21:34:21 -0600

    Moved Java code to new file

---
+++ vip.java
@@ -0,0 +1,30 @@
+public static void main(String[] args) { 
+		System.out.println("Hey guys ");
+		
+import java.util.Scanner;
+public static void main(String[] args) { 
+    System.out.println("What is your name?");
+		
+//type identifier = new type()
+    Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+System.out.println("Hello "+ name);
+
+String string = new String("This is a string");
+String easier = "Wow, that was easy!";
+System.out.println(easier");
+	}
+
+}
+
+passwordFile = open('SecretPasswordFile.txt')
+secretPassword = passwordFile.read()
+print('Enter your password.')
+typedPassword = input()
+if typedPassword == secretPassword:
+	print('Access Granted')
+	if typedPassword == '12345' :
+		print('That password is one that an idiot puts on their luggage.')
+else:
+	print('Access Denied');
