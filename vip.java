a07f04f6d657b612e0cff305ce39524c9a7adaeb
cadewhite15 <52638277+cadewhite15@users.noreply.github.com>
2019-07-17 13:53:10 -0600

    Update vip.java

--- vip.java
+++ vip.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class VIP
 {
 
-	publi static void main(String[] args)
+	public static void main(String[] args)
 	{
 		System.out.println("Hey guys ");
 		System.out.println("What is your name?");
